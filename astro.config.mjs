73a8f4ae1164ed175a1eb091d96b5b21a6c060ba
Eugene de Beste <eugene@debeste.co.za>
2026-03-25 10:00:51 +0200

    fix paths for dev and prod

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   site: 'https://eugene.debeste.co.za',
-  base: '/markdown-cv/',
   output: 'static',
   publicDir: './static',
   vite: {
